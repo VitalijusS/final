91cda071fb26f4f454ca8bf31a5cee5ac5fdb523
VitalijusS <v.stifanovicius@gmail.com>
2024-09-11 14:45:12 +0300

    db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
+import { env } from './env.js'
 import { apiRouter } from './router/api.js';
 
 const app = express();
-const port = 5020;
 
 app.use(express.json({ type: 'application/json' }));
 app.use(express.urlencoded({ extended: true }))
@@ -13,8 +13,8 @@ app.all('*', (req, res) => {
     return res.json({ status: 'error', message: "Wrong page" });
 })
 
-app.listen(port, () => {
-    console.log(`Turizmo serveris: http://localhost:${port}`);
+app.listen(env.SERVER_PORT, () => {
+    console.log(`Turizmo serveris: http://localhost:${env.SERVER_PORT}`);
 });
 
 app.use((req, res, next) => {
