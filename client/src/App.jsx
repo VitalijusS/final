48723a1503563f6c0f2ea7277f53174801a8ebbe
VitalijusS <v.stifanovicius@gmail.com>
2024-09-11 12:33:28 +0300

    links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { LocationListing } from './pages/LocationListing';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/locations' element={<LocationListing />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
